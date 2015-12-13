668ccf7ec6175f234764f49d4c1d55e7213180e1
yzy <yzylivezh@hotmail.com>
2015-12-13 21:40:31 +0800

    foxnews crawler: add tech and science support

--- crawler/crawler_fox/crawler_fox/spiders/spider_fox.py
+++ crawler/crawler_fox/crawler_fox/spiders/spider_fox.py
@@ -16,28 +16,22 @@ class spider_yahoo(scrapy.Spider):
         #'http://www.foxnews.com/us/2015/12/13/one-person-shot-inside-pennsylvania-wal-mart/'
         "http://m.foxnews.com/us?intcmp=subnav", 
         "http://m.foxnews.com/world?intcmp=subnav", 
-        #"http://www.foxnews.com/science.html?intcmp=subnav", 
-        #"http://www.foxnews.com/tech.html?intcmp=subnav"
+        "http://www.foxnews.com/science.html?intcmp=subnav", 
+        "http://www.foxnews.com/tech.html?intcmp=subnav"
     ]   
 
-    news_link_xpath_us_world = '//*[@id="wrapper"]/section/div/article[1]/h3/a/@href'
-    news_link_xpath_sci = '//*[@id="content"]/div[3]/section/ul/li[3]/article/h3/a'
-    news_link_xpath_tech = ''
+    news_link_xpath_us_world = '//*[@id="wrapper"]/section/div/article/h3/a/@href'
+    news_link_xpath_sci = '//*[@id="content"]/div/section/ul/li/article/h3/a/@href'
+    news_link_xpath_tech = '//*[@id="content"]/div/section/ul/li/article/h2/a/@href'
 
-    news_comment_entry_xpath_us_world = '//*[@id="comment_header"]/div/table/tr/td/div/a/@href'
-    news_comment_entry_xpath_tech_sci = ''
+    news_title_xpath = '//*[@id="content"]/div/div//article/div/h1/text()'
+    
+    news_article_xpath = '//*[@id="content"]/div/div//article/div/div//p/text()'
 
-    news_title_xpath_us_world = '//*[@id="content"]/div/div/div[2]/div/div[3]/article/div/h1/text()'
-    news_title_xpath_tech_sci = ''
+    news_comment_button_xpath = '//*[@id="livefyre_comment_stream"]/div[1]/div/div[7]/div[3]/div'
 
-    news_article_xpath_us_world = '//*[@id="content"]/div/div/div[2]/div/div[3]/article/div/div[3]/p/text()'
-    news_article_xpath_tech_sci = ''
-
-    news_comment_button_xpath_us_world = '//*[@id="livefyre_comment_stream"]/div[1]/div/div[7]/div[3]/div'
-    news_comment_button_xpath_tech_sci = ''
-
-    news_comment_xpath_us_world_list = ['//*[@id="livefyre_comment_stream"]/div[1]/div/div[7]/div[1]/article/div/section/div/p', 
-                                        '//*[@id="livefyre_comment_stream"]/div[1]/div/div[7]/div[1]/article/div/div/article/div[1]/section/div/p']
+    news_comment_xpath_list = ['//*[@id="livefyre_comment_stream"]/div[1]/div/div[7]/div[1]/article/div/section/div/p', 
+                               '//*[@id="livefyre_comment_stream"]/div[1]/div/div[7]/div[1]/article/div/div/article/div[1]/section/div/p']
 
     # news_list
     # containg news{title:'', link:'', article:'', comments:[comment]}
@@ -49,11 +43,17 @@ class spider_yahoo(scrapy.Spider):
         #print response.url
         if response.url.find("world") != -1 or response.url.find("us") != -1:
             for link in response.xpath(self.news_link_xpath_us_world).extract():
+            #    print response.urljoin(link)
                 yield scrapy.Request(response.urljoin(link), callback=self.parseNews)
         elif response.url.find("tech") != -1:
-            print "not implemented"
+            for link in response.xpath(self.news_link_xpath_tech).extract():
+                complete_link = response.urljoin(link)
+                if complete_link.find("http://www.foxnews.com/tech") != -1 and \
+                   complete_link.find("slideshow") == -1:
+                    yield scrapy.Request(complete_link, callback=self.parseNews)
         else:
-            print "not implemented"
+            for link in response.xpath(self.news_link_xpath_sci).extract():
+                yield scrapy.Request(response.urljoin(link), callback=self.parseNews)
         #    yield scrapy.Request(response.urljoin(link), callback=self.parseNews)
  
     def parseNews(self, response):
@@ -62,14 +62,20 @@ class spider_yahoo(scrapy.Spider):
         news_element['link'] = response.url;
         
         # fetch news_title
-        news_title = response.xpath(self.news_title_xpath_us_world).extract()[0]
+        news_title = response.xpath(self.news_title_xpath).extract()[0]
         news_element['title'] = news_title
+
+        #print news_title
+        #return
         
         # fetch news_article
         news_article = ''
-        for news_paragraph in response.xpath(self.news_article_xpath_us_world).extract():
+        for news_paragraph in response.xpath(self.news_article_xpath).extract():
             news_article += news_paragraph + '\n'
         news_element['article'] = news_article
+
+        #print news_article
+        #return
         
         # fetch news_comments
         news_element['comments'] = []
@@ -90,14 +96,14 @@ class spider_yahoo(scrapy.Spider):
             while click_retry_count < 3: 
                 try:
                     time.sleep(5)
-                    show_comments = browser.find_element_by_xpath(self.news_comment_button_xpath_us_world)
+                    show_comments = browser.find_element_by_xpath(self.news_comment_button_xpath)
                     show_comments.click()
                 except:
                     click_retry_count += 1
                     continue
 
         # now we get comments from the final page
-        for xpath in self.news_comment_xpath_us_world_list:
+        for xpath in self.news_comment_xpath_list:
             for comment in browser.find_elements_by_xpath(xpath):
                 news_element['comments'].append(comment.text)
 
