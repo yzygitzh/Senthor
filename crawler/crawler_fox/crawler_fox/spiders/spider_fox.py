08df0dd6bcdf20ac791cf7a675fd2dced41619f2
yzy <yzylivezh@hotmail.com>
2015-12-22 14:16:43 +0800

    chrome -> phantomJS

--- crawler/crawler_fox/crawler_fox/spiders/spider_fox.py
+++ crawler/crawler_fox/crawler_fox/spiders/spider_fox.py
@@ -5,6 +5,7 @@ import json
 import sys
 # deal with dynamic contents
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 
 reload(sys)
@@ -81,9 +82,14 @@ class spider_yahoo(scrapy.Spider):
         news_element['comments'] = []
 
         # selenium comes to rescue!
-        options = webdriver.ChromeOptions()
-        options.add_argument('Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5')
-        browser = webdriver.Chrome(chrome_options=options)
+        #options = webdriver.ChromeOptions()
+        #options.add_argument('Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5')
+        #browser = webdriver.Chrome(chrome_options=options)
+        dcap = dict(DesiredCapabilities.PHANTOMJS)
+        dcap["phantomjs.page.settings.userAgent"] = (
+            "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5"
+        ) 
+        browser = webdriver.PhantomJS(desired_capabilities=dcap)
 
         get_timeout = 0
         browser.set_page_load_timeout(90)
